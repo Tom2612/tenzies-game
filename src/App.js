ad745cb4ec57d605f6da453a0955560c97a0ee48
Tom <tomcat-26@hotmail.co.uk>
2022-10-16 08:17:19 +0100

    added dice functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Die from './Die';
 import './style.css';
 import {nanoid} from 'nanoid';
 
 function App() {
+
+  const [dice, setDice] = useState(allNewDice())
+
+  function generateNewDie() {
+    return {
+      value: Math.ceil(Math.random() * 6),
+      isHeld: false,
+      id: nanoid()
+    }
+  }
+
+  function allNewDice() {
+    const newDice = [];
+    for (let i = 0; i < 10; i++){
+      newDice.push(generateNewDie())
+    }
+    return newDice;
+  }
+
+  const diceElements = dice.map(die => (
+    <Die 
+      key={die.id}
+      value={die.value}
+      isHeld={die.isHeld}
+      holdDice={() => holdDice(die.id)}
+    />
+  ))
+
   return (
     <main>
       <h1 className="title">Tenzies</h1>
       <p className='instructions'>Roll until all dice are the same. 
         Click each die to freeze it at its current value between rolls.</p>
         <div className='dice-container'>
-          <Die />
-          <Die />
-          <Die />
-          <Die />
-          <Die />
-          <Die />
-          <Die />
-          <Die />
-          <Die />
-          <Die />
+          {diceElements}
         </div>
         <button className='roll-dice'>Roll</button>
     </main>
